318f85b35f7064f02033453cc8435323b4bccbac
peterlam <peter.lam@dreamshub.com>
2022-09-21 13:13:31 +0800

    custom loading Image Icon

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,29 @@
 import React from "react"
+import { Outlet } from "react-router-dom"
+import { Header } from "./Header"
+import { Leftside } from "./Leftside"
+import { PageLoader } from "./PageLoader"
+import { Rightside } from "./Rightside"
 
 
-type LayoutProps = React.ComponentProps<"div">
+type LayoutProps = React.ComponentProps<"div"> &{
+    loaderImage?:string,
+
+}
 
 
-const CustomHexaBitTemplateLayout=(props:LayoutProps)=>{
-    return <div></div>
+const CustomHexaBitTemplateLayout = (props: LayoutProps) => {
+    return <>
+        <PageLoader loaderImage={props.loaderImage}/>
+        <div id="wrapper">
+            <div className="overlay" />
+            <Header />
+            <Rightside />
+            <Leftside />
+            {/* mainContent */}
+            <Outlet />
+
+        </div></>
 }
 
 export default CustomHexaBitTemplateLayout
